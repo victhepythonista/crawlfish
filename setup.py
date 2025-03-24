c47f86d31c3a343934dbe16ab3e2589afc30d4e5
victhepythonista <letingvictorkipkemboi@gmail.com>
2025-03-24 21:04:07 +0300

    Setup.py added , license added

---
+++ setup.py
@@ -0,0 +1,29 @@
+ 
+
+import setuptools
+from setuptools import setup, find_packages
+
+long_description = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="crawlfish",
+    version="0.0.1",
+    author="Victor Kipkemboi",
+    author_email="scriptilapia@gmail.com",
+    long_description =long_description,
+    long_description_content_type="text/markdown",
+    description="""An easy to use web crawling library for exploring whole websites and webpages , element searching , handy bs4 related shortcuts , generic web scraping functions  and  more  """,
+    url="https://github.com/victhepythonista/crawlfish",
+    project_urls={
+        "Bug Tracker": "https://github.com/victhepythonista/crawlfish/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    packages=["art_showcase"],
+    python_requires=">=3.6",
+ 
+)
